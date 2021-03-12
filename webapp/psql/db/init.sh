9a25f09e922f87aa8798bded30ae0b59a4ebad11
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-03-12 10:35:52 +0900

    refactor DB initialization

--- webapp/psql/db/init.sh
+++ webapp/psql/db/init.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-set -xe
-set -o pipefail
+set -ue -o pipefail
 
 CURRENT_DIR=$(cd $(dirname $0);pwd)
-export PG_HOST=${PG_HOST:-127.0.0.1}
-export PG_PORT=${PG_PORT:-5432}
-export PG_USER=${PG_USER:-isucon}
-export PG_DBNAME=${PG_DBNAME:-isuumo}
+source ${HOME}/env.sh
 export LANG="C.UTF-8"
+
 cd ${CURRENT_DIR}
 
-dropdb --if-exists isuumo
-createdb isuumo
-psql -h ${PG_HOST} -p ${PG_PORT} -U ${PG_USER} -f "0_Schema.sql" -f "1_DummyEstateData.sql" -f "2_DummyChairData.sql" ${PG_DBNAME}
+if ! psql -q -h ${PG_HOST} -p ${PG_PORT} -U ${PG_USER} -d ${PG_DBNAME} -c "\q" > /dev/null 2>&1; then
+  createdb ${PG_DBNAME}
+fi
+psql -h ${PG_HOST} -p ${PG_PORT} -U ${PG_USER} -d ${PG_DBNAME} \
+  -f "0_Schema.sql" \
+  -f "1_DummyEstateData.sql" \
+  -f "2_DummyChairData.sql"
